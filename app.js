d1c3148e29aeed2f3aa19ee99900f15a5a1d9b72
Mayank Goyal <f2014768@pilani.bits-pilani.ac.in>
2015-01-19 22:48:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(function(){
 		this.clickCount=ko.observable(0);
 		this.name= ko.observable("Tom");
 		this.imgSrc=ko.observable("cat_picture1.jpg");
-		this.imgAttribute=ko.observable("http://media.photobucket.com/user/michelleNpete/media/BaBas/awesome-beautiful-blue-eyes-cat-cute-Favimcom-110476.jpg.html?filters[term]=cat&filters[primary]=images&filters[featured%5Fmedia]=1220&filters[secondary]=videos&sort=1&o=0");
+		this.imgAttribute=ko.observable("(https://cloud.githubusercontent.com/assets/10373048/5804536/c3be2a50-a02c-11e4-8d58-a818aa839caa.jpg)");
 		this.nickNames=ko.observableArray(["Billa","Meow"]);
 		this.title=ko.computed(){
 			var title;
